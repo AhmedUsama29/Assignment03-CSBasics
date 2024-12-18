675869b20da7d44ed9e3ca4ce7db80fc5686a790
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2024-12-18 03:07:09 +0200

    Solved Q3

--- Assignment 03/Program.cs
+++ Assignment 03/Program.cs
@@ -18,22 +18,61 @@
 
             #region Question 02: Write a program to allow the user to enter int and print the REVERSED of it.
 
-            Console.WriteLine("Enter the int:");
-            String Text = Console.ReadLine();
-            bool flag = int.TryParse(Text, out int number);
+            //Console.WriteLine("Enter the int:");
+            //String Text = Console.ReadLine();
+            //bool flag = int.TryParse(Text, out int number);
 
-            if (flag)
+            //if (flag)
+            //{
+            //    char[] TextArr = Text.ToCharArray();
+            //    for (int i = TextArr.Length - 1; i >= 0; i--)
+            //    {
+            //        Console.Write(TextArr[i]);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Please enter a valid number");
+            //}
+            #endregion
+
+            #region Question 03:Write a program in C# Sharp to find prime numbers within a range of numbers.
+
+            Console.WriteLine("Enter the start Number:");
+            string Start = Console.ReadLine();
+            bool flag1 = int.TryParse(Start, out int start);
+
+            Console.WriteLine("Enter the end Number:");
+            string End = Console.ReadLine();
+            bool flag2 = int.TryParse(End, out int end);
+
+            if (flag1 && flag2)
             {
-                char[] TextArr = Text.ToCharArray();
-                for (int i = TextArr.Length - 1; i >= 0; i--)
+                Console.WriteLine("Prime numbers in the range are:");
+                for (int i = start; i <= end; i++)
                 {
-                    Console.Write(TextArr[i]);
+                    int count = 0;
+
+                    for (int j = 1; j <= i; j++) 
+                    {
+                        if (i % j == 0)
+                        {
+                            count++;
+                        }
+                    }
+
+                    if (count == 2) 
+                    {
+                        Console.WriteLine(i);
+                    }
                 }
             }
             else
             {
-                Console.WriteLine("Please enter a valid number");
+                Console.WriteLine("Invalid input. Please enter valid integers.");
             }
+
+
             #endregion
         }
     }
