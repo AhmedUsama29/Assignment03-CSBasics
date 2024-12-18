5566e5041e2583ee42f6a8d1208b47e16f05fc1d
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2024-12-18 02:51:07 +0200

    Solved Q1

--- Assignment 03/Program.cs
+++ Assignment 03/Program.cs
@@ -2,9 +2,21 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 01: Write a program to allow the user to enter a string and print the REVERSE of it.
+
+            Console.WriteLine("Enter the string:");
+            String Text = Console.ReadLine();
+            char[] TextArr = Text.ToCharArray();
+            for (int i = TextArr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(TextArr[i]);
+            }
+
+            #endregion Question 01: Write a program to allow the user to enter a string and print the REVERSE of it.
+
+
         }
     }
 }
