9fbd5fe0e24e16716d0425538bf1e142ab98ce63
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2024-12-18 02:55:55 +0200

    Solved Q2

--- Assignment 03/Program.cs
+++ Assignment 03/Program.cs
@@ -6,17 +6,35 @@
         {
             #region Question 01: Write a program to allow the user to enter a string and print the REVERSE of it.
 
-            Console.WriteLine("Enter the string:");
-            String Text = Console.ReadLine();
-            char[] TextArr = Text.ToCharArray();
-            for (int i = TextArr.Length - 1; i >= 0; i--)
-            {
-                Console.Write(TextArr[i]);
-            }
+            //Console.WriteLine("Enter the string:");
+            //String Text = Console.ReadLine();
+            //char[] TextArr = Text.ToCharArray();
+            //for (int i = TextArr.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write(TextArr[i]);
+            //}
 
             #endregion Question 01: Write a program to allow the user to enter a string and print the REVERSE of it.
 
+            #region Question 02: Write a program to allow the user to enter int and print the REVERSED of it.
 
+            Console.WriteLine("Enter the int:");
+            String Text = Console.ReadLine();
+            bool flag = int.TryParse(Text, out int number);
+
+            if (flag)
+            {
+                char[] TextArr = Text.ToCharArray();
+                for (int i = TextArr.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(TextArr[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+            #endregion
         }
     }
 }
